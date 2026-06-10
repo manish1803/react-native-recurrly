890a648615d6f4328c22297bebcdb01a69436989
Manish Patil <patilmanishp18@gmail.com>
2026-06-10 09:32:22 +0530

    implement the home page

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { Text } from "react-native";
-
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,6 +1,5 @@
-import { Text } from "react-native";
-
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
